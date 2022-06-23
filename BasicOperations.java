24910175e8326e4d6e3bf1327f98cbeb3d0e9fb5
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-06-23 10:25:12 +0300

    BasicMathematicalOperations Solution in Codewars
    https://www.codewars.com/kata/57356c55867b9b7a60000bd7

---
+++ BasicOperations.java
@@ -0,0 +1,18 @@
+public class BasicOperations
+{
+  public static Integer basicMath(String op, int v1, int v2){
+   
+    int x = 0;
+    
+    if(op.equals("+")) {
+      return v1+v2;
+    } else if (op.equals("-")) {
+      return v1-v2;
+    } else if (op.equals("*")) {
+      return v1*v2;
+    } else {
+      return v1/v2;
+    }
+    
+  }
+}
