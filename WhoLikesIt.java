238bef77e1c40320dc338c09c150c921e9bb71ce
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-07-29 15:43:54 +0300

    WhoLikesIt Solution in Codewars
    https://www.codewars.com/kata/5266876b8f4bf2da9b000362

---
+++ WhoLikesIt.java
@@ -0,0 +1,24 @@
+public class WhoLikesIt {
+
+	public static String whoLikesIt( String... names ) {
+		
+      int length = names.length;
+
+      if ( length == 0 ) 
+      	return "no one likes this";
+
+      else if ( length == 1 ) 
+      	return names[0] + " likes this";
+
+      else if ( length == 2 ) 
+        	return names[0] + " and " + names[1] + " like this";
+
+      else if ( length == 3 ) 
+      	return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+
+      else 
+      	return names[0] + ", " + names[1] + " and " + (length-2) +" others like this";
+        
+    }
+
+}
