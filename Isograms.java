43155f37075fddef8f0cbb8ceb1a4483fb9536fa
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-08 17:06:33 +0300

    Isogram Solution in Codewars
    https://www.codewars.com/kata/54ba84be607a92aa900000f1

---
+++ Isograms.java
@@ -0,0 +1,24 @@
+public class Isogram {
+
+    public static boolean  isIsogram(String str) {
+
+        if( str.length() == 0 ) 
+            return true;
+
+        for ( int i = 0 ; i < str.length() ; i++ ) {
+
+            for ( int j = i + 1 ; j < str.length() ; j++ ) {
+
+                if ( str.charAt(i) == str.charAt(j) || str.substring( i , i + 1 ).equals(str.substring( j , j + 1 ).toUpperCase() ) || str.substring( i , i + 1 ).equals(str.substring( j , j + 1 ).toLowerCase() ) ) 
+                    return false;
+                
+            }
+
+        }
+
+        
+        return true;
+    
+    } 
+
+}
