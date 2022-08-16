f2a2cc2320cc358d4140a623b6f58d32125b4a74
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-16 12:02:10 +0300

    GrasshopperSummation Solution in Codewars
    https://www.codewars.com/kata/55d24f55d7dd296eb9000030

---
+++ GrasshopperSummation.java
@@ -0,0 +1,15 @@
+public class GrasshopperSummation {
+
+	public static int summation(int n) {
+
+      int sum = 0;
+
+      for ( int i = 1 ; i <= n ; i++ ) {
+        sum += i;
+      }
+
+      return sum;
+
+    }
+
+}
